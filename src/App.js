f2c942f734ee49d1b90490236ba356afddba0140
WadihEA <wadihatieh9@gmail.com>
2023-11-02 12:58:27 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import articles from './data'; // Import the articles array
 
-function App() {
+const SearchComponent = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Search articles..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <ul>
+        {articles.map((article) => {
+          const searchRegex = new RegExp(searchTerm, 'gi');
+          const highlightedTitle = article.title.replace(searchRegex, (match) => `<mark>${match}</mark>`);
+          const highlightedContent = article.content.replace(searchRegex, (match) => `<mark>${match}</mark>`);
+
+          if (!highlightedTitle.includes('<mark>') && !highlightedContent.includes('<mark>')) {
+            return null; // Skip rendering the article if it doesn't contain the search term
+          }
+
+          return (
+            <li key={article.id}>
+              <strong dangerouslySetInnerHTML={{ __html: highlightedTitle }} /><br />
+              <span dangerouslySetInnerHTML={{ __html: highlightedContent }} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default SearchComponent;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+const articles = [
+    {
+      id: 1,
+      title: "Introduction to React",
+      content: "React is a JavaScript library for building user interfaces.",
+    },
+    {
+      id: 2,
+      title: "Getting Started with Vue.js",
+      content: "Vue.js is a progressive JavaScript framework for building modern web applications.",
+    },
+    {
+      id: 3,
+      title: "The Basics of Angular",
+      content: "Angular is a platform for building mobile and desktop web applications.",
+    },
+    {
+      id: 4,
+      title: "Node.js Fundamentals",
+      content: "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
+    },
+    {
+      id: 5,
+      title: "Python Programming Guide",
+      content: "Python is a popular programming language known for its simplicity and readability.",
+    },
+    {
+      id: 6,
+      title: "JavaScript ES6 Features",
+      content: "ES6 introduced many new features and syntax enhancements to JavaScript.",
+    },
+    {
+      id: 7,
+      title: "HTML and CSS Best Practices",
+      content: "Learn the best practices for writing clean and maintainable HTML and CSS code.",
+    },
+    {
+      id: 8,
+      title: "Responsive Web Design Techniques",
+      content: "Explore various techniques for creating responsive web designs that work well on different devices.",
+    },
+  ];
+  
+  export default articles;
